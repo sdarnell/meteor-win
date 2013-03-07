SprayHank/meteor

New facts package: a proof-of-concept implementation of server-side statistics.
Publishes statistics from the server to a Minimongo collection,
Meteor.Facts.server. (On the server side, Facts is *not* backed by MongoDB.) By
default, this is available to all users if autopublish is on, or no users if it
is not on, but you can configure this with Meteor.Facts.setUserIdFilter.

You can add an ugly view on the stats to your app with {{> serverFacts }}.

Current stats, by package:

- mongo-livedata:
  - observe-handles
  - live-results-sets (when this is less than observe-handles, cursor de-dup is helping you)
- livedata
  - subscriptions
  - crossbar-listeners
  - sessions-attached
  - sessions-detached
  - sessions (should be sum of sessions-attached and sessions-detached)

packages/facts/facts.js
@@ -1,0 +1,81 @@
+(function(){
+
+Meteor.Facts = {};
+
+var serverFactsCollection = 'Facts.server';
+
+if (Meteor.isServer) {
+  var autopublishIsOn = false;
+  Meteor.default_server.onAutopublish(function () {
+    autopublishIsOn = true;
+  });
+
+  // By default, we publish facts to no user if autopublish is off, and to all
+  // users if autopublish is on.
+  var userIdFilter = function (userId) {
+    return autopublishIsOn;
+  };
+
+  // XXX make this take effect at runtime too?
+  Meteor.Facts.setUserIdFilter = function (filter) {
+    userIdFilter = filter;
+  };
+
+  // XXX Once we implement observeChanges, we should be able to use a minimongo
+  // collection here and hook up an observeChanges directly to a publish.
+  var factsByPackage = {};
+  var activeSubscriptions = [];
+
+  Meteor.Facts.incrementServerFact = function (pkg, fact, increment) {
+    if (!_.has(factsByPackage, pkg)) {
+      factsByPackage[pkg] = {};
+      factsByPackage[pkg][fact] = increment;
+      _.each(activeSubscriptions, function (sub) {
+        sub.added(serverFactsCollection, pkg, factsByPackage[pkg]);
+      });
+      return;
+    }
+
+    var packageFacts = factsByPackage[pkg];
+    if (!_.has(packageFacts, fact))
+      factsByPackage[pkg][fact] = 0;
+    factsByPackage[pkg][fact] += increment;
+    var changedField = {};
+    changedField[fact] = factsByPackage[pkg][fact];
+    _.each(activeSubscriptions, function (sub) {
+      sub.changed(serverFactsCollection, pkg, changedField);
+    });
+  };
+
+  // XXX Also publish facts-by-package.
+  Meteor.publish("facts", function () {
+    var sub = this;
+    if (!userIdFilter(this.userId)) {
+      sub.complete();
+      return;
+    }
+    activeSubscriptions.push(sub);
+    _.each(factsByPackage, function (facts, pkg) {
+      sub.added(serverFactsCollection, pkg, facts);
+    });
+    sub.onStop(function () {
+      activeSubscriptions = _.without(activeSubscriptions, sub);
+    });
+    sub.complete();
+  });
+} else {
+  Meteor.Facts.server = new Meteor.Collection(serverFactsCollection);
+  Meteor.subscribe("facts");
+  Template.serverFacts.factsByPackage = function () {
+    return Meteor.Facts.server.find();
+  };
+  Template.serverFacts.facts = function () {
+    var factArray = [];
+    _.each(this, function (value, name) {
+      if (name !== '_id')
+        factArray.push({name: name, value: value});
+    });
+    return factArray;
+  };
+}
+})();

sdarnell/meteor-win

packages/facts/facts.js
@@ -1,0 +1,81 @@
+(function(){
+
+Meteor.Facts = {};
+
+var serverFactsCollection = 'Facts.server';
+
+if (Meteor.isServer) {
+  var autopublishIsOn = false;
+  Meteor.default_server.onAutopublish(function () {
+    autopublishIsOn = true;
+  });
+
+  // By default, we publish facts to no user if autopublish is off, and to all
+  // users if autopublish is on.
+  var userIdFilter = function (userId) {
+    return autopublishIsOn;
+  };
+
+  // XXX make this take effect at runtime too?
+  Meteor.Facts.setUserIdFilter = function (filter) {
+    userIdFilter = filter;
+  };
+
+  // XXX Use a minimongo collection instead and hook up an observeChanges
+  // directly to a publish.
+  var factsByPackage = {};
+  var activeSubscriptions = [];
+
+  Meteor.Facts.incrementServerFact = function (pkg, fact, increment) {
+    if (!_.has(factsByPackage, pkg)) {
+      factsByPackage[pkg] = {};
+      factsByPackage[pkg][fact] = increment;
+      _.each(activeSubscriptions, function (sub) {
+        sub.added(serverFactsCollection, pkg, factsByPackage[pkg]);
+      });
+      return;
+    }
+
+    var packageFacts = factsByPackage[pkg];
+    if (!_.has(packageFacts, fact))
+      factsByPackage[pkg][fact] = 0;
+    factsByPackage[pkg][fact] += increment;
+    var changedField = {};
+    changedField[fact] = factsByPackage[pkg][fact];
+    _.each(activeSubscriptions, function (sub) {
+      sub.changed(serverFactsCollection, pkg, changedField);
+    });
+  };
+
+  // XXX Also publish facts-by-package.
+  Meteor.publish("facts", function () {
+    var sub = this;
+    if (!userIdFilter(this.userId)) {
+      sub.ready();
+      return;
+    }
+    activeSubscriptions.push(sub);
+    _.each(factsByPackage, function (facts, pkg) {
+      sub.added(serverFactsCollection, pkg, facts);
+    });
+    sub.onStop(function () {
+      activeSubscriptions = _.without(activeSubscriptions, sub);
+    });
+    sub.ready();
+  });
+} else {
+  Meteor.Facts.server = new Meteor.Collection(serverFactsCollection);
+  Meteor.subscribe("facts");
+  Template.serverFacts.factsByPackage = function () {
+    return Meteor.Facts.server.find();
+  };
+  Template.serverFacts.facts = function () {
+    var factArray = [];
+    _.each(this, function (value, name) {
+      if (name !== '_id')
+        factArray.push({name: name, value: value});
+    });
+    return factArray;
+  };
+}
+})();