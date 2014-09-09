3ffb5c33af83323b0c90c7568af2d7a6a434666d
Stephen Darnell <stephen@darnell.plus.com>
2014-09-09 09:47:27 +0100

    Create windows variant of npm-node-aes-gcm

--- packages/non-core/npm-node-aes-gcm/package.js
+++ packages/non-core/npm-node-aes-gcm/package.js
@@ -1,12 +1,13 @@
 Package.describe({
   summary: "Wrapper around the node-aes-gcm npm package",
-  version: '0.1.3'
+  version: '0.1.3',
+  name: 'windows:npm-node-aes-gcm'
 });
-
+/*
 Npm.depends({
   'node-aes-gcm': '0.1.3'
 });
-
+*/
 Package.on_use(function (api) {
   api.export('NpmModuleNodeAesGcm', 'server');
   api.addFiles('wrapper.js', 'server');
